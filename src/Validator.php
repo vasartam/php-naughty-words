3d60a3d34b02e9706f5c6ac5086a2612322376c1
Василий Артамонов <artamonovvasya10@gmail.com>
2022-07-15 13:31:54 +0300

    Add functionality for returning naughty words found

--- src/Validator.php
+++ src/Validator.php
@@ -31,7 +31,21 @@ class Validator
         }
     }
 
-    public function hasNaughtyWords(string $text): bool
+    /**
+     * Finds naughty words in the given text and returns them in the array of this shape:
+     * ```php
+     *  [
+     *      'ru' => false,
+     *      'en' => 'word',
+     *  ]
+     * ```
+     * where `false` means naughty words were not found and string 'word'
+     *  signifies a naughty word that was found for respective language.
+     *
+     * @param string $text
+     * @return array
+     */
+    public function findNaughtyWords(string $text): array
     {
         $matchResults = [];
 
@@ -40,11 +54,35 @@ class Validator
 
             $matchResult = preg_match($regex, $text, $matches);
 
-            $matchResults[$language] = (bool)$matchResult;
+            // If the match is found, assign the word or `true`.
+            // If not, assign `false`.
+            if ($matchResult === 1) {
+                $matchResultToReturn = $matches['word'] ?? true;
+            } else {
+                $matchResultToReturn = false;
+            }
+
+            $matchResults[$language] = $matchResultToReturn;
         }
 
-        // If at least one value is `true`, return `true`
-        if (in_array(true, array_values($matchResults), true)) {
+        return $matchResults;
+    }
+
+    /**
+     * Determines whether the given text has naughty words or not.
+     *
+     * @param string $text
+     * @return bool
+     */
+    public function hasNaughtyWords(string $text): bool
+    {
+        $matchResults = $this->findNaughtyWords($text);
+
+        // Filter falsy values.
+        $matchResultsTruthy = array_filter(array_values($matchResults));
+
+        // At this point, if there is at least one value in the `$matchResults` array, there are naughty words.
+        if (count($matchResultsTruthy) > 0) {
             return true;
         }
 
